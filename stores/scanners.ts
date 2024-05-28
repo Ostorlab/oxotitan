01b3888159474a2fda83221e167f291dfd207b29
benyissa <benyissa@ostorlab.dev>
2024-05-28 12:52:15 +0100

    JsDocs

--- stores/scanners.ts
+++ stores/scanners.ts
@@ -1,33 +1,61 @@
 import { defineStore } from 'pinia'
 
+/**
+ * Represents a Scanner object.
+ */
 export interface Scanner {
   endpoint: string
   apiKey: string
   name: string
 }
+
+/**
+ * Represents the state of the scanners store.
+ */
 interface ScannersState {
   scanners: Scanner[]
 }
 
+/**
+ * Represents the actions of the scanners store.
+ */
 interface ScannersActions {
+  /**
+   * Adds a new scanner or updates an existing scanner.
+   * @param scanner - The scanner to be added or updated.
+   */
   addOrUpdateScanner(scanner: Scanner): void
+
+  /**
+   * Deletes a scanner by its endpoint.
+   * @param endpoint - The endpoint of the scanner to be deleted.
+   */
   deleteScanner(endpoint: string): void
 }
 
 export const useScannersStore = defineStore<'scanners', ScannersState, Record<string, never>, ScannersActions>('scanners', {
-  state: () => ({
+  state: (): ScannersState => ({
     scanners: [] as Scanner[]
   }),
   actions: {
-    addOrUpdateScanner(scanner: Scanner) {
+    /**
+     * Adds a new scanner or updates an existing scanner.
+     * @param scanner - The scanner to be added or updated.
+     */
+    addOrUpdateScanner(scanner: Scanner): void {
       const index = this.scanners.findIndex((_scanner) => _scanner.endpoint === scanner.endpoint)
-      if (index == -1)
+      if (index === -1) {
         this.scanners.push(scanner)
-      else {
+      } else {
         this.scanners[index] = scanner
       }
     },
-    deleteScanner(endpoint: string) {
+
+    /**
+     * Deletes a scanner by its endpoint.
+     * @param endpoint - The endpoint of the scanner to be deleted.
+     */
+    deleteScanner(endpoint: string): void {
       const index = this.scanners.findIndex((scanner) => scanner.endpoint === endpoint)
       if (index !== -1) {
         this.scanners.splice(index, 1)
