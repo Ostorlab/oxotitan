01fb37e9661071b1542f1cf5ea121d21e4436a31
rabsondev <rabson@ostorlab.dev>
2024-06-05 08:56:35 +0100

    Add breadcrumbs to other pages

--- pages/scan/new.vue
+++ pages/scan/new.vue
@@ -1,13 +1,43 @@
 <template>
-  <CreateScanForm />
+  <div>
+    <DfBreadcrumbs
+      :breadcrumbs="breadcrumbs"
+      class="mb-5"
+    />
+    <CreateScanForm />
+  </div>
 </template>
 
 <script lang="ts">
+import { DfBreadcrumbs } from '~/dragonfly/components/Sections/DfBreadcrumbs'
+import type { VulnerabilityDetailBreadcrumbsType } from '~/dragonfly/components/Sections/DfBreadcrumbs/types'
 import CreateScanForm from '~/scan/components/form/CreateScanForm.vue'
+
+interface Data {
+  breadcrumbs: VulnerabilityDetailBreadcrumbsType
+}
 
 export default defineComponent({
   components: {
-    CreateScanForm
+    CreateScanForm,
+    DfBreadcrumbs
+  },
+  data(): Data {
+    return {
+      breadcrumbs: [
+        {
+          text: 'scanning',
+          disabled: true,
+          exact: true
+        },
+        {
+          text: 'New Scan',
+          disabled: true,
+          to: '/scan/new',
+          exact: true
+        }
+      ]
+    }
   }
 })
 </script>
