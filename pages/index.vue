6711ed220c1eb40baefefdc05dec448fa00423c0
rabsondev <rabson@ostorlab.dev>
2024-06-06 15:00:28 +0100

    Redirect the user to the appropriate page (scan list or scanners)

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>Hello OXO</div>
+  <div>OXO</div>
 </template>
 
 <script setup lang="ts">
+import { useScannersStore } from '~/stores/scanners'
 
+const scannersStore = useScannersStore()
+const scanners = scannersStore.scanners
+
+onMounted(() => {
+  if (scanners.length === 0) {
+    navigateTo('/scanners')
+  } else {
+    navigateTo('/scan/list')
+  }
+})
 </script>
-
-<style scoped>
-
-</style>
