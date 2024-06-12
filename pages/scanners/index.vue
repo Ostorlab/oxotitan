d3f4bc61fea2a4ca11c6aafa641ce1024e14690c
mohsinenar <mnarmouche@gmail.com>
2024-06-12 18:16:18 +0100

    Set local scanner as default scanner

--- pages/scanners/index.vue
+++ pages/scanners/index.vue
@@ -19,7 +19,8 @@
         </v-btn>
         <ScannerForm
           v-else
-          @close-form="showForm = false"
+          :scanner="currentScanner"
+          @close-form="closeForm"
         />
       </v-card-text>
     </v-card>
@@ -32,8 +33,25 @@ import ScannerForm from '~/project/scanners/components/ScannerForm.vue'
 import ScannersList from '~/project/scanners/components/ScannersList.vue'
 import { DfBreadcrumbs } from '~/dragonfly/components/Sections/DfBreadcrumbs'
 import type { VulnerabilityDetailBreadcrumbsType } from '~/dragonfly/components/Sections/DfBreadcrumbs/types'
+import type { Scanner } from '~/project/types'
 
 const showForm = ref(false)
+
+const route = useRoute()
+const currentScanner = ref<Scanner>({ endpoint: '', apiKey: '', name: '' })
+onBeforeMount(() => {
+  const endpoint = route.query.endpoint
+  const apiKey = route.query.key
+  const name = 'Local machine'
+  if (endpoint !== undefined && endpoint !== null && apiKey !== undefined && apiKey !== null) {
+    showForm.value = true
+    currentScanner.value = {
+      endpoint: endpoint.toString(),
+      apiKey: apiKey.toString(),
+      name
+    }
+  }
+})
 
 const breadcrumbs: VulnerabilityDetailBreadcrumbsType = [
   {
@@ -43,4 +61,16 @@ const breadcrumbs: VulnerabilityDetailBreadcrumbsType = [
     exact: true
   }
 ]
+
+/**
+ * Close the form and reset the current scanner.
+ */
+function closeForm() {
+  showForm.value = false
+  currentScanner.value = {
+    endpoint: '',
+    apiKey: '',
+    name: ''
+  }
+}
 </script>
