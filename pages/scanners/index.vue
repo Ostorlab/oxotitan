e5087f46e745ec616a7a17199454bedeaa941462
mohsinenar <mnarmouche@gmail.com>
2024-06-13 10:26:29 +0100

    Add a new scanner page.

--- pages/scanners/index.vue
+++ pages/scanners/index.vue
@@ -13,15 +13,10 @@
           v-if="showForm === false"
           color="accent"
           prepend-icon="mdi-plus"
-          @click="showForm = !showForm"
+          @click="router.push('/scanners/new')"
         >
           New
         </v-btn>
-        <ScannerForm
-          v-else
-          :scanner="currentScanner"
-          @close-form="closeForm"
-        />
       </v-card-text>
     </v-card>
     <ScannersList />
@@ -29,30 +24,12 @@
 </template>
 
 <script setup lang="ts">
-import ScannerForm from '~/project/scanners/components/ScannerForm.vue'
 import ScannersList from '~/project/scanners/components/ScannersList.vue'
 import { DfBreadcrumbs } from '~/dragonfly/components/Sections/DfBreadcrumbs'
 import type { VulnerabilityDetailBreadcrumbsType } from '~/dragonfly/components/Sections/DfBreadcrumbs/types'
-import type { Scanner } from '~/project/types'
 
 const showForm = ref(false)
-
-const route = useRoute()
-const currentScanner = ref<Scanner>({ endpoint: '', apiKey: '', name: '' })
-onBeforeMount(() => {
-  const endpoint = route.query.endpoint
-  const apiKey = route.query.key
-  const name = 'Local machine'
-  if (endpoint !== undefined && endpoint !== null && apiKey !== undefined && apiKey !== null) {
-    showForm.value = true
-    currentScanner.value = {
-      endpoint: endpoint.toString(),
-      apiKey: apiKey.toString(),
-      name
-    }
-  }
-})
-
+const router = useRouter()
 const breadcrumbs: VulnerabilityDetailBreadcrumbsType = [
   {
     text: 'Scanners',
@@ -61,16 +38,4 @@ const breadcrumbs: VulnerabilityDetailBreadcrumbsType = [
     exact: true
   }
 ]
-
-/**
- * Close the form and reset the current scanner.
- */
-function closeForm() {
-  showForm.value = false
-  currentScanner.value = {
-    endpoint: '',
-    apiKey: '',
-    name: ''
-  }
-}
 </script>
