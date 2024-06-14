daef9b39604dfef60cbc6b1af057320fe7e395d2
rabsondev <rabson@ostorlab.dev>
2024-06-14 11:43:30 +0100

    Add Alpha tag

--- project/common/components/NavigationAppBar.vue
+++ project/common/components/NavigationAppBar.vue
@@ -25,6 +25,13 @@
       alt="OXO logo"
       style="height:30px; object-fit: contain"
     >
+    <v-chip
+      size="x-small"
+      class="ml-2"
+      color="accent"
+    >
+      Aplha
+    </v-chip>
     <template #append>
       <div class="d-flex align-center ml-2">
         <v-btn
