35f11b63031bca7a85f508f9aea66ef9c1d61ca3
elyousfi <mohamed.elyousfi@ostorlab.dev>
2024-07-19 16:45:58 +0100

    Add ability to add scanner from new scan page.

--- project/scanners/components/ScannerSelect.vue
+++ project/scanners/components/ScannerSelect.vue
@@ -1,52 +1,109 @@
 <template>
-  <v-select
-    v-model="selectedScanner"
-    :items="scanners"
-    :item-props="getScannerDetials"
-    hide-details
-    variant="outlined"
-    item-title="endpoint"
-    density="compact"
-    clearable
-    return-object
-    label="Select a scanner"
-    placeholder="Select or create a scanner to run the scan on"
-    prepend-icon="mdi-shield-search"
-  >
-    <template #selection="{ item }">
-      <p
-        v-if="(item.raw.name || '').trim() !== ''"
-        class="mb-0"
+  <div>
+    <v-select
+      v-if="scanners.length > 0"
+      v-model="scanner"
+      :items="scanners"
+      :item-props="getScannerDetails"
+      hide-details
+      variant="outlined"
+      item-title="endpoint"
+      density="compact"
+      clearable
+      return-object
+      label="Select a scanner"
+      placeholder="Select or create a scanner to run the scan on"
+      prepend-icon="mdi-shield-search"
+    >
+      <template #selection="{ item }">
+        <p
+          v-if="(item.raw.name || '').trim() !== ''"
+          class="mb-0"
+        >
+          {{ item.raw.name }} (<code>{{ item.raw.endpoint }}</code>)
+        </p>
+        <code
+          v-else
+          class="mb-0"
+        >{{ item.raw.endpoint }}</code>
+      </template>
+    </v-select>
+    <div v-else>
+      <v-btn
+        color="primary"
+        @click="showNewScannerForm = true"
       >
-        {{ item.raw.name }} (<code>{{ item.raw.endpoint }}</code>)
-      </p>
-      <code
-        v-else
-        class="mb-0"
-      >{{ item.raw.endpoint }}</code>
-    </template>
-  </v-select>
+        <v-icon start>
+          mdi-plus
+        </v-icon> New Scanner
+      </v-btn>
+    </div>
+    <v-dialog
+      v-model="showNewScannerForm"
+      persistent
+      max-width="600px"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="headline">Create New Scanner</span>
+        </v-card-title>
+        <v-card-text>
+          <ScannerForm @close-form="showNewScannerForm = false" />
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script lang="ts">
 import { mapStores } from 'pinia'
 import type { Scanner } from '~/project/types'
 import { useScannersStore } from '~/stores/scanners'
+import ScannerForm from '~/project/scanners/components/ScannerForm.vue'
 
 interface Data {
-  selectedScanner: Scanner | null
+  scanner: Scanner | null
   scanners: Array<Scanner>
+  showNewScannerForm: boolean
 }
+
 export default defineComponent({
   name: 'ScannerSelect',
+  components: {
+    ScannerForm
+  },
+  props: {
+    selectedScanner: {
+      type: Object as () => Scanner | null,
+      default: null
+    }
+  },
+  emits: ['update:selectedScanner'],
   data(): Data {
     return {
-      selectedScanner: null,
-      scanners: []
+      scanner: this.selectedScanner,
+      scanners: [],
+      showNewScannerForm: false
     }
   },
   computed: {
     ...mapStores(useScannersStore)
+  },
+  watch: {
+    /**
+     * Update the selected scanner when the scanner changes.
+     * @param newSelectedScanner The new scanner.
+     */
+    selectedScanner(newSelectedScanner) {
+      this.scanner = newSelectedScanner
+    },
+    /**
+     * Emit the selected scanner when the scanner changes.
+     * @param newSelectedScanner The new scanner.
+     */
+    scanner(newSelectedScanner) {
+      this.$emit('update:selectedScanner', newSelectedScanner)
+    }
   },
   mounted() {
     this.scanners = this.scannersStore?.scanners || []
@@ -56,11 +113,11 @@ export default defineComponent({
      * Get the scanner details.
      * @param scanner The scanner whose details to get.
      */
-    getScannerDetials(scanner: Scanner): { title: string, subtitle: string } {
+    getScannerDetails(scanner: Scanner): { title: string, subtitle: string } {
       const scannerHasName = (scanner.name || '').trim() !== ''
       return {
-        title: scannerHasName === true ? scanner.name : scanner.endpoint,
-        subtitle: scannerHasName === true ? scanner.endpoint : ''
+        title: scannerHasName ? scanner.name : scanner.endpoint,
+        subtitle: scannerHasName ? scanner.endpoint : ''
       }
     }
   }
